a7d07c8626cc2225545d81473c1b12cf52ec72cd
rahulanand-181 <rahulanand181@gmail.com>
2021-06-26 20:52:01 +0530

    ajaxcall

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const request = new XMLHttpRequest();
-request.open('GET',"https://restcountries.eu/rest/v2/name/india");
+request.open('GET',"https://restcountries.eu/rest/v2/name/Japan");
 request.send();
 console.log(request.responseText);
 request.addEventListener("load",function(){
-    console.log(JSON.parse(this.responseText));
+    let data = JSON.parse(this.responseText)[0];
+    let currency = data.currencies[0]
+    let secondData = JSON.parse(this.responseText)[1];
+    console.log(data);
     const html= `
         <div class="container">
           <div class="card">
-                <img src="https://restcountries.eu/data/ind.svg" alt="flag" >
+                <img src="${data.flag}" alt="flag" >
                     <div class="details">
-                        <h4>country name</h4>
-                        <p>continent name</p>
-                        <p>population</p>
+                        <h4>Country: ${data.name}</h4>
+                        <p>Capital: ${data.capital}</p>
+                        <p>Currency: ${currency.symbol}</p> 
+                        <span>${currency.name}</span>
+                        <p>Continent: ${data.region}</p>
+                        <p>Population : ${data.population}</p>
                     </div> 
            </div>
         </div> `
+        let startingDom = document.getElementById('start');
+        startingDom.insertAdjacentHTML('afterend', html);
   })
